e92d8aa5656afdb4757937c94d8254d99f9c2c07
Ariya Adinatha <ariyaadinatha21@gmail.com>
2026-07-27 09:48:15 +0700

    chore: add domain and remove duplicate assets

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,12 +2,18 @@
 #
 # Check for changed posts
 
+# Posts live in the `security-notes` repo, checked out into `_posts/` by the
+# deploy workflow, so git has to be run inside that nested repo rather than in
+# this one -- from here the paths are gitignored and every lookup returns 0.
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  dir = File.dirname(post.path)
+  file = File.basename(post.path)
+
+  commit_num = `git -C "#{ dir }" rev-list --count HEAD -- "#{ file }"`
 
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+    lastmod_date = `git -C "#{ dir }" log -1 --pretty="%ad" --date=iso -- "#{ file }"`
     post.data['last_modified_at'] = lastmod_date
   end
 
